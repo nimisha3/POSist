1ec4451636014b23bab78b3b22cc13693b00211f
nimisha3 <30775138+nimisha3@users.noreply.github.com>
2018-08-30 19:19:44 +0530

    Add files via upload

---
+++ POSist Client.java
@@ -0,0 +1,15 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Client
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		DynamicList list = new DynamicList();
+		list.display();
+	}
+}
